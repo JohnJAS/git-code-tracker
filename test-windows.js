6f4fe70777c8c4993738a5b7b31ac91220cd6a56
yooocen <605529354@qq.com>
2026-05-17 10:00:54 +0800

    test: add cube, factorial, fibonacci, isPalindrome test functions

--- test-windows.js
+++ test-windows.js
@@ -7,3 +7,29 @@ function testFn() {
 function square(n) {
   return n * n;
 }
+
+function cube(n) {
+  return n * n * n;
+}
+
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+function fibonacci(n) {
+  if (n <= 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log("square(4):", square(4));
+console.log("cube(3):", cube(3));
+console.log("factorial(5):", factorial(5));
+console.log("fibonacci(7):", fibonacci(7));
+console.log("isPalindrome('racecar'):", isPalindrome("racecar"));
